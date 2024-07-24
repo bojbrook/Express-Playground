da265d68757c98f515638862c6de9d57e83a6b7c
bojbrook <bowen.brooks@datadoghq.com>
2024-07-24 11:41:48 -0600

    Added port values for heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/server');
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is listening on port ${process.env.PORT || 3000}');
 });
